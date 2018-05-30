6ba8c31fd685958d5ba15b0f14cbc967d37d5636
abalazsics <anett.balazsics@digihey.com>
2018-05-30 10:49:36 +0200

    feat: add option to cast to timestamp and remove casting to jsonb[]

--- etl/transform/unnester.py
+++ etl/transform/unnester.py
@@ -36,15 +36,15 @@ def cast_prim(column_type, value):
   if column_type == 'text':
     new_value = str(value)
 
+  elif column_type == 'timestamp':
+    new_value = value
+
   elif column_type == 'double precision':
     new_value = None;
     try:
       new_value = float(str(value))
     except ValueError as e:
       new_value = 'undefined'
-
-  elif column_type == 'jsonb[]':
-    new_value = [json.dumps(v, default=default) for v in value]
 
   elif column_type == 'jsonb':
     new_value = json.dumps(value, default=default)
