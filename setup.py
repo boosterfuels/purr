d65afeb5c453fcfc96f644a3f3040b0615dc2bca
Marko Vrbanec <marko.vrbanec@protonmail.com>
2018-06-04 08:47:51 +0200

    chore: basic setup.py config

--- setup.py
+++ setup.py
@@ -1,2 +1,8 @@
-# This file will contain a global setup() function.
-#
+from setuptools import setup, find_packages
+
+setup(name='purr',
+      version='0.0.1',
+      author='Anett Balázsics',
+      email='anett.balazsics@digihey.com',
+      url='https://github.com/boosterfuels/purr',
+      packages=find_packages())
