fc1a1e338107ad1f92f842a5ad1b11abd8562979
Marko Vrbanec <marko.vrbanec@protonmail.com>
2018-06-06 17:14:18 +0200

    fix: install requirements when installing the package

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from setuptools import setup, find_packages
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(name='purr',
       version='0.0.1',
       author='Anett Balázsics',
       email='anett.balazsics@digihey.com',
       url='https://github.com/boosterfuels/purr',
       packages=find_packages(),
+      install_requires=requirements,
       entry_points={
           'console_scripts': [
               'purr = etl.__main__:main'
