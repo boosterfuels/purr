2d688945dfc6b6095975c0c7ab66fcc5b1069ca7
abalazsics <anett.balazsics@digihey.com>
2018-06-06 14:15:20 +0200

    fix: remove extra options

--- etl/core.py
+++ etl/core.py
@@ -7,7 +7,7 @@ from etl.load import schema
 from etl.extract import init_mongo as mongodb
 from etl.load import init_pg as postgres
 
-def transfer_collections(collections, truncate, drop, settings):
+def transfer_collections(collections, settings, coll_config):
   """
   Parameters
   ----------
@@ -26,7 +26,7 @@ def transfer_collections(collections, truncate, drop, settings):
   setup_mdb = settings['mongo']
   pg = postgres.PgConnection(setup_pg['db_name'], setup_pg['user'])
   mongo = mongodb.MongoConnection(setup_mdb)
-  ex = extractor.Extractor(pg, mongo.conn, setup_pg, settings)
+  ex = extractor.Extractor(pg, mongo.conn, setup_pg, settings, coll_config)
 
   if setup_pg['schema_reset'] is True:
     schema.reset(pg, setup_pg["schema_name"])
@@ -36,18 +36,12 @@ def transfer_collections(collections, truncate, drop, settings):
     ex.transfer_bulk(collections)
     
   if settings['tailing'] is True:
-    start_tailing(start_date_time, pg, mongo.conn, setup_pg, settings)
+    start_tailing(start_date_time, pg, mongo.conn, setup_pg, settings, coll_config)
   else:
     pg.__del__()
     
-def start_tailing(start_date_time, pg, mdb, setup_pg, settings):
+def start_tailing(start_date_time, pg, mdb, setup_pg, settings, coll_config):
   if start_date_time is None:
     start_date_time = datetime.utcnow()
-  t = tailer.Tailer(pg, mdb, setup_pg, settings)
+  t = tailer.Tailer(pg, mdb, setup_pg, settings, coll_config)
   t.start(start_date_time)
-
-def get_collection_names():
-  return collection.get_names()
-
-def reset_schema(conn, schema_name):
-  schema.reset(conn, schema_name)
