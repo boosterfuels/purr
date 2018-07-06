07e10bc459001c4601b1672dc5b7ab5d5e4e2dfc
abalazsics <anett.balazsics@digihey.com>
2018-07-06 15:31:33 +0200

    refct(cli, core):  remove unused code, rename functions, adding comments

--- etl/cli.py
+++ etl/cli.py
@@ -16,13 +16,11 @@ def valid_date(s):
 
 def main():
     parser = argparse.ArgumentParser(
-        description = '''An ETL tool for your MongoDB and Postgres.'''
+        description = '''An ETL tool to transfer data from MongoDB to Postgres.'''
     )
     
-    parser.add_argument('-lc', '--list-colls', action='store_true', dest='list_colls', help='List all collections in MongoDB.')
-    parser.add_argument('-c', '--colls', type=str,  dest='mdb_colls', help='MongoDB collections which will be transfered. Case sensitive!', nargs='+')
-    parser.add_argument('-sf', '--setup-file', type=str,  dest='path_setup_file', help='Basic setup like schema name, collections to transfer.')
-    parser.add_argument('-cf', '--collection-file', type=str, dest='path_collection_file', help='Path to the file that should contain collection names with corresponding field names MongoDB and PG.')
+    parser.add_argument('-sf', '--setup-file', type=str,  dest='path_setup_file', help='Path to YAML file bith basic setup: \nconnection string for MongoDB and PG, schema name, schema reset, collections to transfer, etc.')
+    parser.add_argument('-cf', '--collection-file', type=str, dest='path_collection_file', help='Path to YAML file that contains information about collections: collection names, field names, types, PG relation and attribute names, etc.')
     args = parser.parse_args()
 
     if len(sys.argv) <= 1:
@@ -37,7 +35,4 @@ def main():
 
         if setup_file and coll_file:
             settings_postgres = setup_file["postgres"]
-            colls = setup_file["collections"]
-            if len(colls) == 0:
-                colls = core.get_collection_names()
-            etl.core.transfer_collections(colls, setup_file, coll_file)
+            etl.core.start(setup_file, coll_file)
--- etl/core.py
+++ etl/core.py
@@ -4,24 +4,37 @@ from datetime import datetime
 
 from etl.extract import collection, extractor, tailer, init_mongo as mongodb, transfer_info
 from etl.load import schema, init_pg as postgres
+from etl.monitor import logger
 
-def transfer_collections(collections, settings, coll_config):
+def start(settings, coll_config):
     """
-  Parameters
-  ----------
-  collections
-  truncate
-  drop
-  Example
-  -------
-  transfer_collections(['Feedback', 'Vehicle', 'Customer', 'Terminal'])
-  TODO
-  ----
-  - create table with attributes and types
-  """
+    Starts 
+    Returns
+    -------
+    -
+
+    Parameters
+    ----------    
+    settings : dict
+             : basic settings for both PG and MongoDB (connection strings, schema name)
+
+    coll_config : dict
+                : config file for collections
+
+    TODO
+    ----
+    - create table with attributes and types
+    """
 
     setup_pg = settings["postgres"]
     setup_mdb = settings["mongo"]
+    # setup_file["collections"] contains list of collections
+    # which will be transferred
+    collections = settings["collections"]
+    
+    if collections is None:
+        logger.error("No collections found. Check your collection names in the setup file.")
+        return
 
     pg = postgres.PgConnection(setup_pg)
     schema.create(pg, setup_pg["schema_name"])
@@ -41,7 +54,7 @@ def transfer_collections(collections, settings, coll_config):
     if ex.typecheck_auto is True:
         ex.transfer_auto(collections)
     else:
-        ex.transfer_bulk(collections)
+        ex.transfer_conf(collections)
 
     if settings["tailing"] is True:
         t.start(start_date_time)
