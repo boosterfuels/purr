0e5338de360b53ff8898c2e2d8280564c301cfcb
abalazsics <anett.balazsics@digihey.com>
2018-09-05 13:09:18 +0200

    feat: add paths to gulpfile; replace index.html

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,26 @@
-var gulp        = require('gulp');
-var deploy      = require('gulp-gh-pages');
+var gulp = require('gulp')
+var deploy = require('gulp-gh-pages')
+var del = require('del')
+
+var paths = {
+  src: 'src/**/*',
+  srcHTML: 'src/**/*.html',
+  srcCSS: 'src/**/*.css',
+  srcJS: 'src/**/*.js',
+  tmp: 'tmp',
+  tmpIndex: 'tmp/index.html',
+  tmpCSS: 'tmp/**/*.css',
+  tmpJS: 'tmp/**/*.js',
+  dist: 'dist',
+  distIndex: 'dist/index.html',
+  distCSS: 'dist/**/*.css',
+  distJS: 'dist/**/*.js'
+};
+
 
 /**
  * Push build to gh-pages
  */
-gulp.task('deploy', function () {
-  return gulp.src("./dist/**/*")
-    .pipe(deploy())
-});
+gulp.task('deploy', function() {
+  return gulp.src('./dist/**/*').pipe(deploy())
+})
