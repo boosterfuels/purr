d62dd28044d750bda6432f6b5e8ac382d5a113af
abalazsics <anett.balazsics@digihey.com>
2019-03-29 21:18:47 +0100

    Update 2019-03-29T20:18:44.800Z

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-console.log("Purr!")
+console.log('Purr!')
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {
+  scrollFunction()
+}
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById('btn-sticky').style.display = 'block'
+  } else {
+    document.getElementById('btn-sticky').style.display = 'none'
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0
+  document.documentElement.scrollTop = 0
+}
