e6cb1f26b7066618d35bf8e63aa9d1fe35ce072a
abalazsics <anett.balazsics@digihey.com>
2019-04-11 20:59:52 +0200

    test(tailer, row): add new tests

---
+++ tests/extract/test_tailer.py
@@ -0,0 +1,34 @@
+from etl.extract import collection
+from etl.extract import tailer
+import unittest
+from tests.meta import mock
+from bson import ObjectId
+
+
+class TestTailer(unittest.TestCase):
+    def test_prepare_docs_for_update_one(self):
+        docs = [{'coll_name': 'Company',
+                 'db_name': 'test_purrito',
+                 'o': {'$set': {'active': True, 'name': True}},
+                 'o2': {'_id': ObjectId('5caf5998a54d758375bd9928')},
+                 'op': 'u'}]
+        mocked = [{'_id': '5caf5998a54d758375bd9928',
+                   'active': True, 'name': True}]
+        docs_useful = tailer.prepare_docs_for_update(docs)
+        assert mocked == docs_useful
+
+    def test_prepare_docs_for_update_multiple_merged(self):
+        docs = [{'coll_name': 'Company',
+                 'db_name': 'test_purrito',
+                 'o': {'$set': {'active': True, 'name': True}},
+                 'o2': {'_id': ObjectId('5caf5998a54d758375bd9928')},
+                 'op': 'u'},
+                {'coll_name': 'Company',
+                 'db_name': 'test_purrito',
+                 'o': {'$set': {'name': 'stuffy'}, '$unset': {'active': True}},
+                 'o2': {'_id': ObjectId('5caf5998a54d758375bd9928')},
+                 'op': 'u'}]
+        mocked = [{'_id': '5caf5998a54d758375bd9928',
+                   'active': '$unset', 'name': 'stuffy'}]
+        docs_useful = tailer.prepare_docs_for_update(docs)
+        assert mocked == docs_useful
--- tests/load/test_row.py
+++ tests/load/test_row.py
@@ -1,4 +1,4 @@
-from etl.load import row
+from etl.load import row, constraint
 import unittest
 from tests.meta import mock
 
@@ -61,3 +61,119 @@ class TestRow(unittest.TestCase):
         print(res)
         cursor.close()
         assert tuple(values[2]) == res
+
+    def test_upsert_bulk(self):
+        reset_dataset_pg()
+        schema = 'public'
+        table = 'employee'
+        attrs = ["id", "first_name", "last_name", "hair"]
+        constraint.add_pk(pg, schema, table, attrs[0])
+        values = [
+            ["12345", "Purr", "Rito", "orange"],
+            ["12346", "James", "Cat", "black and white"],
+            ["12347", "Morgo", None, "black and white"],
+            ["12348", "Voros", "Macska", "orange"],
+            ["12349", "Szurke", "Macska", "grey"]
+        ]
+        row.insert_bulk(pg, schema, table, attrs, values)
+        values_new = [
+            ["12346", "James", "Cat", "black and white"],
+            ["12347", "Morgo", None, "black and white"],
+            ["12348", "Zuta", "Macska", "yellow"],  # changed
+            ["12349", "Szurke", "Cicus", "grey"],  # changed
+            ["12350", "Fekete", "Cica", "black"]  # new
+        ]
+        row.upsert_bulk(pg, schema, table, attrs, values_new)
+        cmd = "select * from %s" % table
+        # cmd = "select * from %s where id = '%s'" % (table, values[5][0])
+        cursor = pg.conn.cursor()
+        cursor.execute(cmd)
+        res = cursor.fetchall()
+        mocked = [
+            ["12345", "Purr", "Rito", "orange"],
+            ["12346", "James", "Cat", "black and white"],
+            ["12347", "Morgo", None, "black and white"],
+            ["12348", "Zuta", "Macska", "yellow"],
+            ["12349", "Szurke", "Cicus", "grey"],
+            ["12350", "Fekete", "Cica", "black"]
+        ]
+        mocked = [tuple(x) for x in mocked]
+        cursor.close()
+        assert mocked == res
+
+    def test_upsert_bulk_tail(self):
+        reset_dataset_pg()
+        schema = 'public'
+        table = 'employee'
+        attrs = ["id", "first_name", "last_name", "hair"]
+        constraint.add_pk(pg, schema, table, attrs[0])
+        values = [
+            ["12345", "Purr", "Rito", "orange"],
+            ["12346", "James", "Cat", "black and white"],
+            ["12347", "Morgo", None, "black and white"],
+            ["12348", "Voros", "Macska", "orange"],
+            ["12349", "Szurke", "Macska", "grey"]
+        ]
+        row.insert_bulk(pg, schema, table, attrs, values)
+        values_new = [
+            ["12346", "James", "Cat", "black and white"],
+            ["12347", "Morgo", None, "black and white"],
+            ["12348", "Zuta", "Macska", "yellow"],  # changed
+            ["12349", "Szurke", "Cicus", "grey"],  # changed
+            ["12350", "Fekete", "Cica", "black"]  # new
+        ]
+        row.upsert_bulk_tail(pg, schema, table, attrs, values_new)
+        cmd = "select * from %s" % table
+        cursor = pg.conn.cursor()
+        cursor.execute(cmd)
+        res = cursor.fetchall()
+        mocked = [
+            ["12345", "Purr", "Rito", "orange"],
+            ["12346", "James", "Cat", "black and white"],
+            ["12347", "Morgo", None, "black and white"],
+            ["12348", "Zuta", "Macska", "yellow"],
+            ["12349", "Szurke", "Cicus", "grey"],
+            ["12350", "Fekete", "Cica", "black"]
+        ]
+        mocked = [tuple(x) for x in mocked]
+        cursor.close()
+        assert mocked == res
+
+    
+    def test_upsert_bulk_tail_unset(self):
+        reset_dataset_pg()
+        schema = 'public'
+        table = 'employee'
+        attrs = ["id", "first_name", "last_name", "hair"]
+        constraint.add_pk(pg, schema, table, attrs[0])
+        values = [
+            ["12345", "Purr", "Rito", "orange"],
+            ["12346", "James", "Cat", "black and white"],
+            ["12347", "Morgo", None, "black and white"],
+            ["12348", "Voros", "Macska", "orange"],
+            ["12349", "Szurke", "Macska", "grey"]
+        ]
+        row.insert_bulk(pg, schema, table, attrs, values)
+        values_new = [
+            ["12346", "James", "Cat", "black and white"],
+            ["12347", "Morgo", None, "black and white"],
+            ["12348", "Zuta", "Macska", "yellow"],  # changed
+            ["12349", "Szurke", "$unset", "grey"], # unset
+            ["12350", "$unset", "Cica", "$unset"] # unset
+        ]
+        row.upsert_bulk_tail(pg, schema, table, attrs, values_new)
+        cmd = "select * from %s" % table
+        cursor = pg.conn.cursor()
+        cursor.execute(cmd)
+        res = cursor.fetchall()
+        mocked = [
+            ["12345", "Purr", "Rito", "orange"],
+            ["12346", "James", "Cat", "black and white"],
+            ["12347", "Morgo", None, "black and white"],
+            ["12348", "Zuta", "Macska", "yellow"],
+            ["12349", "Szurke", None, "grey"],
+            ["12350", None, "Cica", None]
+        ]
+        mocked = [tuple(x) for x in mocked]
+        cursor.close()
+        assert mocked == res
