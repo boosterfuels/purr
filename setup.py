aba2954109cacfe14520ef2e26dbe113246c968f
abalazsics <anett.balazsics@digihey.com>
2019-04-15 11:51:01 +0200

    chore: update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
-version = '0.1.14'
+version = '0.1.15'
 
 setup(name='purr',
       version=version,
