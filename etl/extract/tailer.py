536e0095096231582d3db88a96ca24ef3e5a50a6
abalazsics <anett.balazsics@digihey.com>
2019-04-29 12:48:52 +0200

    feat: log tailed rows

--- etl/extract/tailer.py
+++ etl/extract/tailer.py
@@ -104,6 +104,31 @@ class Tailer(extractor.Extractor):
             logger.error("%s Deatils %s" % (CURR_FILE, ex))
             return False
 
+    def log_tailed_docs(self, docs_useful, ids_log, r, oper, merged):
+        log_entries = []
+        ts = time.time()
+        logger.info("IDs: %s\n" % ids_log)
+        for i in range(len(ids_log)):
+            id = ids_log[i]
+            doc = "no entry"
+            try:
+                if docs_useful[i] is not None:
+                    doc = str(docs_useful[i])
+                else:
+                    doc = "Doc is NULL"
+            except Exception as ex:
+                logger.error("%s Converting log entry failed. Details: %s\n Document: " %
+                             (CURR_FILE, ex))
+                logger.error(docs_useful[i])
+            row = [oper, r.relation_name, id, ts,
+                   merged, doc]
+            log_row = tuple(row)
+            log_entries.append(log_row)
+        try:
+            transfer_info.log_rows(self.pg, self.schema, log_entries)
+        except Exception as ex:
+            logger.error("%s Logging failed. Details: %s" % (CURR_FILE, ex))
+
     def flush(self, docs, oper, r):
         """
         sends all the data which was collected for one
@@ -156,25 +181,7 @@ class Tailer(extractor.Extractor):
                     """%s Deleting multiple documents failed: %s.
                     Details: %s""" % (CURR_FILE, docs, ex))
 
-        log_entries = []
-        ts = time.time()
-        for i in range(len(ids_log)):
-            id = ids_log[i]
-            doc = None
-            try:
-                doc = str(docs_useful[i])
-            except Exception as ex:
-                logger.error("%s Converting log entry failed. Details: %s\n Document: " %
-                             (CURR_FILE, ex))
-                logger.error(docs_useful[i])
-            row = [oper, r.relation_name, id, ts,
-                   merged, doc]
-            log_row = tuple(row)
-            log_entries.append(log_row)
-        try:
-            transfer_info.log_rows(self.pg, self.schema, log_entries)
-        except Exception as ex:
-            logger.error("%s Logging failed. Details: %s" % (CURR_FILE, ex))
+        self.log_tailed_docs(docs_useful, ids_log, r, oper, merged)
 
     def transform_and_load_many(self, docs_details):
         """
